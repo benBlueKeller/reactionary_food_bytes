a6128004afa28cec1c832f0ae2f95e8d4d21692b
benBlueKellet <benblue22@gmail.com>
2016-10-22 22:55:59 -0700

    added AddItem form and corresponding css-not yet used

--- app.jsx
+++ app.jsx
@@ -33,6 +33,41 @@ Item.propTypes = {
 	name: React.PropTypes.string.isRequired,
 };
 
+var AddItemForm = React.createClass({
+  propTypes: {
+    onAdd: React.PropTypes.func.isRequired,
+  },
+  
+  getInitialState: function() {
+    return {
+      name: "",
+    };
+  },
+  
+  onNameChange: function(e) {
+    this.setState({name: e.target.value});
+  },
+  
+  onSubmit: function(e) {
+    e.preventDefault();
+  
+    this.props.onAdd(this.state.name);
+    this.setState({name: ""});
+  },
+  
+  
+  render: function() {
+    return (
+      <div className="add-item-form">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} onChange={this.onNameChange} />
+          <input type="submit" value="Add Item" />
+        </form>
+      </div>
+    ); 
+  }
+});
+
 var Application = React.createClass({
 	propTypes: {
 		title: React.PropTypes.string,
@@ -58,9 +93,9 @@ var Application = React.createClass({
 			<div className="tile">
 			<Header title={this.props.title} />
 			<div className="items">
-				{this.state.food.map(function(item) {
+				{this.state.food.map(function(item, index) {
 					return(
-						<Item name={item.name}/>
+						<Item name={item.name} key={index}/>
 						);
 				}.bind(this))}
 			</div>

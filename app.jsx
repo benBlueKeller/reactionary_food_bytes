4394324ebb98c61097756b9e6b0593ffd1f708da
benBlueKellet <benblue22@gmail.com>
2016-11-02 06:07:33 -0700

    Counter on each Item in Cart now calls changeExpDate

--- app.jsx
+++ app.jsx
@@ -242,6 +242,12 @@ var Cart = React.createClass({
     console.log(this.state.food[index])
   },
 
+  createDateString: function(index) {
+    var d = this.state.food[index].expDate || false;
+    var months = ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec'];
+    return d ? months[d.getMonth()] + ' ' + d.getDate() : 'who kno'
+  },
+
   render: function() {
     return (
       <div className = "tile">
@@ -250,7 +256,11 @@ var Cart = React.createClass({
             {this.state.food.map(function(item, index) {
               return(
                 // TODO:: as you think about data structures, find better keys
-                <Item name={item.name} onRemove={function() {this.props.onItemAdd(item)}.bind(this)} key={item.ndbno}/>
+                <Item name={item.name} 
+                onRemove={function() {this.props.onItemAdd(item)}.bind(this)} 
+                onChange={function(delta) {this.changeExpDate(index, delta)}.bind(this)} 
+                qty={this.createDateString(index)}
+                key={item.ndbno}/>
                 );
             }.bind(this))}
           </div>

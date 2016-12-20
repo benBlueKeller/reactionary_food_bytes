6bff4f855955923655e7d192bad7616cd8585a9e
benBlueKellet <benblue22@gmail.com>
2016-12-20 04:44:16 -0800

    concat is failing. trying npm for a change

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@ var reload       = browserSync.reload;
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 
-var jsGlob = 'src/js/*.{js,jsx}'
+var node_modulesG = process.env.HOME + '/AppData/Roaming/npm/node_modules'
+var jsGlob = 'src/js/*.{js,jsx}';
+var jsFiles = ['src/js/app.jsx'];
+
+
 
 // Lint JS/JSX files
 gulp.task('eslint', function() {
@@ -28,15 +32,38 @@ gulp.task('eslint', function() {
     .pipe(eslint.failAfterError());
 });
 
+
+// Copy react.js and react-dom.js to assets/js/src/vendor
+// only if the copy in node_modules is "newer"
+gulp.task('copy-react', function() {
+  return gulp.src(node_modulesG + '/react/dist/react.js')
+    .pipe(newer('vendor/react.js'))
+    .pipe(gulp.dest('vendor'));
+});
+gulp.task('copy-react-dom', function() {
+  return gulp.src(node_modulesG + '/react-dom/dist/react-dom.js')
+    .pipe(newer('vendor/react-dom.js'))
+    .pipe(gulp.dest('vendor'));
+});
+
+// Copy assets/js/vendor/* to assets/js
+gulp.task('concat-js-vendor', ['copy-react', 'copy-react-dom'], function() {
+  return gulp
+    .src([
+      'vendor/react.js',
+      'vendor/react-dom.js'
+    ])
+    .pipe(concat('vendor-bundle.js'))
+    .pipe(gulp.dest('public'));
+});
+
+
 // Concatenate jsFiles.vendor and jsFiles.source into one JS file.
 // Run eslint before concatenating
-gulp.task('concat', [/*'copy-react', 'copy-react-dom', */'eslint'], function() {
-  return gulp.src(['vendor/react.js', 'vendor/react-dom.js', jsGlob])
+gulp.task('concat', ['eslint'], function() {
+  return gulp.src(jsFiles)
     .pipe(sourcemaps.init())
     .pipe(babel({
-      only: [
-        'src/js',
-      ],
       compact: false
     }))
     .pipe(concat('bundle.js'))
@@ -62,4 +89,5 @@ gulp.task('browsersync', function() {
   });
 });
 
+gulp.task('build', ['concat-js-vendor', 'concat'])
 gulp.task('default', []);

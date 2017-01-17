bdcaa716405ae63aa1a3bd1a4d844bc0cd9c30ca
benBlueKellet <benblue22@gmail.com>
2017-01-17 00:17:05 -0800

    added recipe to initial-state

--- created-app/src/initial-state.js
+++ created-app/src/initial-state.js
@@ -11,6 +11,23 @@ const initialState = {
 			qty: 24
 		}
 	],
+
+	recipes: [
+    {
+      name: 'Thai Yellow Curry',
+      food: [
+        {
+          name: "Mae Ploy Yellow Curry Paste",
+          ndbno: "45103142",
+          qty: 24
+        },
+        {
+          name: "Coconut Milk",
+          ndbno: "45086905",
+          qty: 402
+        },
+      ],
+    }]
 }
 
 export default initialState;

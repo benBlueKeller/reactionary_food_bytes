fc2539f59ceb6e784bb81b3b627205236087e729
benBlueKellet <benblue22@gmail.com>
2017-01-20 02:24:35 -0800

    cadded SELECT_ITEM to actionTypes/recipes.js

--- created-app/src/actionTypes/recipes.js
+++ created-app/src/actionTypes/recipes.js
@@ -1,3 +1,4 @@
 export const ADD_ITEM = 'recipes/ADD_ITEM';
 export const REMOVE_ITEM = 'recipes/REMOVE_ITEM';
 export const CHANGE_ITEM_QTY = 'recipes/CHANGE_ITEM_QTY';
+export const SELECT_ITEM = 'recipes/SELECT_ITEM';

eb1fae8c3437f66c5ea42dba163146bd8a7af87d
benBlueKellet <benblue22@gmail.com>
2017-01-20 00:28:22 -0800

    added cart actionTypes & cart actions

---
+++ created-app/src/actions/cart.js
@@ -0,0 +1,47 @@
+import * as CartActionTypes from '../actionTypes/cart.js';
+
+export const shopListAddItem = (name, nbdno = "") => {
+	return {
+		type: PantryActionTypes.shopListADD_ITEM,
+		name,
+		nbdno
+	}
+};
+
+export const shopListRemoveItem = index => {
+	return {
+		type: PantryActionTypes.shopListREMOVE_ITEM,
+		index
+	}
+};
+
+export const shopListChangeItemQty = (index, delta) => {
+	return {
+		type: PantryActionTypes.shopListCHANGE_ITEM_QTY,
+		index,
+		delta
+	}
+};
+
+export const basketAddItem = (name, nbdno = "") => {
+	return {
+		type: PantryActionTypes.basketADD_ITEM,
+		name,
+		nbdno
+	}
+};
+
+export const basketRemoveItem = index => {
+	return {
+		type: PantryActionTypes.basketREMOVE_ITEM,
+		index
+	}
+};
+
+export const basketChangeItemQty = (index, delta) => {
+	return {
+		type: PantryActionTypes.basketCHANGE_ITEM_QTY,
+		index,
+		delta
+	}
+};

4f9229deb9be26de14a7243e6014325b88223538
benBlueKellet <benblue22@gmail.com>
2017-01-26 15:36:29 -0800

    deleted unused var FOOD in Pantry

--- src/app.jsx
+++ src/app.jsx
@@ -30,18 +30,6 @@ function foodIsEqual(f, o) {
   }
 }
 
-var FOOD = [
-  {
-    name: "Mae Ploy Yellow Curry Paste",
-    ndbno: "45103142",
-    qty: 402
-  },
-  {
-    name: "Parsnips, raw", 
-    ndbno: "11298",
-    qty: 24
-  }
-];
 
 class Pantry extends Component {
 	static propTypes = {

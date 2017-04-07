b7913403294dbb8efee1e3aeeba966779a7e933b
benBlueKellet <benblue22@gmail.com>
2017-04-06 17:23:27 -0700

    refactored containers/pantry to add id as param in onRemove

--- src/containers/pantry.js
+++ src/containers/pantry.js
@@ -190,7 +190,7 @@ class Pantry extends Component {
   							<Item name={item.name} 
                 qty={item.qty}
                 onChange={function(delta) {changeItemQty(index, delta, item.id)}.bind(this)}
-                onRemove={function() {removeItem(index)}.bind(this)} 
+                onRemove={function() {removeItem(index, item.id)}.bind(this)} 
                 key={typeof item.ndbno !== "undefined" ? item.ndbno : index}/>
   							);
   					}.bind(this))}

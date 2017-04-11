0607b578363df12f72a46698410124b42cd4d6ce
benBlueKellet <benblue22@gmail.com>
2017-04-11 01:44:44 -0700

    added actionType/cart/*/ADD_ID

--- src/actionTypes/cart.js
+++ src/actionTypes/cart.js
@@ -1,7 +1,9 @@
 export const shopList_ADD_ITEM = 'cart/shopList/ADD_ITEM';
+export const shopList_ADD_ID = 'cart/shopList/ADD_ID';
 export const shopList_REMOVE_ITEM = 'cart/shopList/REMOVE_ITEM';
 export const shopList_CHANGE_ITEM_QTY = 'cart/shopList/CHANGE_ITEM_QTY';
 
 export const basket_ADD_ITEM = 'cart/basket/ADD_ITEM';
+export const basket_ADD_ID = 'cart/basket/ADD_ID';
 export const basket_REMOVE_ITEM = 'cart/basket/REMOVE_ITEM';
 export const basket_CHANGE_ITEM_QTY = 'cart/basket/CHANGE_ITEM_QTY';

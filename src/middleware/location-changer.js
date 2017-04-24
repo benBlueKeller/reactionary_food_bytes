750901287860da584ef542fbb41d367097748b7b
benBlueKellet <benblue22@gmail.com>
2017-04-24 14:59:11 -0700

    in location-changer.js fromLocation now includes sub_location and dispatches

--- src/middleware/location-changer.js
+++ src/middleware/location-changer.js
@@ -2,15 +2,29 @@
 
 const locationChanger = store => next => action => {
 	if(action.type.slice(0, 3) === "to/") {
-		var splitType = action.type.split("/");
-		console.log(splitType);
-		var toLocation = splitType[1];
-		var fromLocation = splitType[3];
+		var typeArray = action.type.split("/");
+		for (let i = 0; i < typeArray.length; i++) {
+			if (typeArray[i] === "from") {
+				var fromPosition = i;
+			}
+		}
+		var toLocation = typeArray[1];
+		var fromLocation = "";
+		for (let i = fromPosition + 1; i < typeArray.length; i++) {
+			if (i > fromPosition + 1) fromLocation += "/";
+			fromLocation +=	typeArray[i];
+		}
+		store.dispatch({
+			...action,
+			type: fromLocation + '/REMOVE_ITEM',
+			skipLogging: true
+		});	
 		store.dispatch({
 			...action,
 			type: toLocation + '/ADD_ITEM',
 			skipLogging: true
 		});
+
 	}
 	next(action);
 }
